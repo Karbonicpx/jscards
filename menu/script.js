bf3182a0a5bdc5584923a38d11abcf3741c8fffc
Yatagaru <nap1903@gmail.com>
2025-11-23 20:16:31 -0300

    Flashcard practice page Created
    With responsive behaviour and a cards JSON

---
+++ menu/script.js
@@ -0,0 +1,11 @@
+
+
+
+// Finding practice button by DOM
+const practiceBtn = document.getElementById("practice-button");
+
+
+// Going to practice page
+practiceBtn.addEventListener("click", () =>{
+    window.location.href = "../practice/index.html";
+})
